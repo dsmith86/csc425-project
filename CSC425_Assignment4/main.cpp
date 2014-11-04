8cc4076bd432c9ea6f710c874e9dc7a67375c357
Daniel Smith <danielsmith4483@gmail.com>
2014-11-03 20:14:59 -0600

    Temporarily overwrite verts until I figure out the syntax

--- CSC425_Assignment4/main.cpp
+++ CSC425_Assignment4/main.cpp
@@ -24,6 +24,51 @@ int main(int argc, char *argv[])
 		model cubeModel = cube.modelData();
 		cubeModel.shader = "basic";
 
+		float originX = 0, originY = 0, originZ = 0;
+		float scaleX = 0.5, scaleY = 0.5, scaleZ = 0.5;
+
+		GLfloat verts[36][VECTOR_SIZE] = {
+				{ originX - scaleX, originY - scaleY, originZ - scaleZ }, // LEFT
+				{ originX - scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ + scaleZ }, // RIGHT
+				{ originX + scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ - scaleZ }, // FRONT
+				{ originX + scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ + scaleZ }, // BACK
+				{ originX - scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ - scaleZ }, // TOP
+				{ originX + scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX + scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY + scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ + scaleZ }, // BOTTOM
+				{ originX + scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX + scaleX, originY - scaleY, originZ + scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ - scaleZ },
+				{ originX - scaleX, originY - scaleY, originZ + scaleZ }
+		};
+
+		// I can't get the pointer syntax right, so for now I'm resetting the verts here
+		cubeModel.vertices = *verts;
+
 		model models[1];
 		models[0] = cubeModel;
 
